95a3aaef6f1ee8ab169709b6f4c898bff3b3b02d
김영은 <128370837+zeroeuni@users.noreply.github.com>
2023-09-11 14:36:11 +0900

    오류 수정

--- C/쉘 정렬.c
+++ C/쉘 정렬.c
@@ -1,26 +1,32 @@
-// 쉘 정렬
-include<stdio.h>
+#include<stdio.h>
 
-insertion_sort(int list[], int first, int last, int gap)//gap은 반드시 홀수 
-{
-	int i, j, key;
-	for(i=first+gap; i<=last; i=i+gap)
-		list[j+gap]=list[j];
-	list[j+gap]=key;
-	
+void insertion_sort(int list[], int first, int last, int gap) {
+    int i, j, key;
+    for (i = first + gap; i <= last; i = i + gap) {
+        key = list[i];
+        j = i - gap;
+        while (j >= first && list[j] > key) {
+            list[j + gap] = list[j];
+            j = j - gap;
+        }
+        list[j + gap] = key;
+    }
 }
-int main(
-	int list[]={10, 8, 6, 20, 4, 3, 22, 1, 0, 15, 16}; 
-	int temp, key, i, j, n, gap;
-	n=sizeof(list)/sizeof(int);
-	for(gap=n/2; gap>0; gap=gap/2)
-	{
-		if((gap%2==0)) gap++;
-		for(i=0; i<gap; i++)
-			insertion_sort(list, i, n-1, gap);
-	}
-	
-	
-	for(i=0;i<sizeof(list)/sizeof(int);i++)
-	printf("%d", list[i]);) 
-	return 0;
+
+int main() {
+    int list[] = {10, 8, 6, 20, 4, 3, 22, 1, 0, 15, 16};
+    int temp, key, i, j, n, gap;
+    n = sizeof(list) / sizeof(int);
+
+    for (gap = n / 2; gap > 0; gap = gap / 2) {
+        if ((gap % 2 == 0))
+            gap++;
+        for (i = 0; i < gap; i++)
+            insertion_sort(list, i, n - 1, gap);
+    }
+
+    for (i = 0; i < sizeof(list) / sizeof(int); i++)
+        printf("%d ", list[i]);
+        
+    return 0;
+}
