83331d8b5b761a172d12ea0b3537a9ee03eb89c4
김영은 <128370837+zeroeuni@users.noreply.github.com>
2023-11-16 10:20:12 +0900

    Create 클래스 스택.py

---
+++ Python/클래스 스택.py
@@ -0,0 +1,31 @@
+class Stack:
+    def __init__(self, capacity=5):
+        self.capacity = capacity
+        self.list = [None] * capacity
+        self.top = -1
+
+    def isEmpty(self):
+        return self.top == -1
+
+    def isFull(self):
+        return self.top == self.capacity - 1
+
+    def push(self, e):
+        if not self.isFull():
+            self.top += 1
+            self.list[self.top] = e
+        else:
+            print("스택이 가득 참")
+
+    def pop(self):
+        if not self.isEmpty():
+            print(self.list[self.top])
+            self.top -= 1
+        else:
+            print("스택이 비었다")
+
+    def peek(self):
+        if not self.isEmpty():
+            print(self.list[self.top])
+        else:
+            print("스택이 비었다")
